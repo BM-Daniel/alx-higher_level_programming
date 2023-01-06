ff71000dfb0637ac7bea128bf77bcb616ffcbf10
BM-Daniel <danbabereyir@gmail.com>
2023-01-06 09:21:36 -0800

    Fix error in task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -9,7 +9,7 @@ while left_digit < 10:
         if left_digit == 8 and right_digit == 9:
             print(f"{left_digit:d}{right_digit:d}")
         else:
-            print(f"{left_digit:d}{right_digit:d}", end=", ")
+            print("{:d}{:d}".format(left_digit, right_digit), end=", ")
 
         right_digit += 1
     left_digit += 1
