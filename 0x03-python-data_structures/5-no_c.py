ba63a6035ecda5ad43ebef28436f716f90d1dc54
BM-Daniel <danbabereyir@gmail.com>
2023-01-08 10:54:59 -0800

    Files for task 5

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def no_c(my_string):
+    my_list = list(my_string)
+
+    for i in range(len(my_list)):
+        if my_list[i] == "c" or my_list[i] == "C":
+            my_list[i] = ""
+
+    final = "".join(my_list)
+    return final
