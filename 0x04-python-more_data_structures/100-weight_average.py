e645f773863b5304970013e7c09235d483125e65
BM-Daniel <danbabereyir@gmail.com>
2023-01-15 06:08:12 -0800

    Fix error in advanced task

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def weight_average(my_list=[]):
-    if my_list is False:
+    if not isinstance(my_list, list) or len(my_list) == 0:
         return 0
 
     total, weight, average = 0, 0, 0
