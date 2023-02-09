80df8c643ee91bc892700304c8afc6294132fc07
BM-Daniel <danbabereyir@gmail.com>
2023-02-08 16:54:36 -0800

    Add task 3 file

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+"""
+Write a function that returns True if the object is an instance of, or if the
+object is an instance of a class that inherited from, the specified class ;
+otherwise False.
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    Verify if object is an instance of the class
+
+    Args:
+        obj (any): The object
+        a_class (type): The type to verify with the object
+
+    Returns:
+        True if object is exactly an instance of the specified class
+        Else False
+    """
+
+    if isinstance(obj, a_class):
+        return True
+
+    return False
