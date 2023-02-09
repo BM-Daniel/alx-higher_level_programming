25ebf7e9034a58bfaaa5081b625a7f35e64d835a
BM-Daniel <danbabereyir@gmail.com>
2023-02-08 17:06:07 -0800

    Add task 6 file

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""
+Write a class BaseGeometry (based on 5-base_geometry.py).
+"""
+
+
+class BaseGeometry():
+    """
+    A blueprint for the BaseGeometry class
+    """
+
+    def area(self):
+        """
+        Area function of class
+        """
+
+        raise Exception("area() is not implemented")
