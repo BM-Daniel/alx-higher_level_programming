24988266bb8f6d31c6db393f8b4d087ef0dfe690
BM-Daniel <danbabereyir@gmail.com>
2023-03-08 09:52:12 -0800

    Make changes in base model file to do task 15-17

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 """ A blueprint for Base class """
+import json
 
 
 class Base:
@@ -28,3 +29,48 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """
+        Returns the JSON string representation of list_dictionaries
+
+        Args:
+            list_dictionaries (list): A list of dictionaries
+        """
+
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """
+        This method writes the JSON string representation of list_objs to file
+
+        Args:
+            list_objs (list): A list of insatnces who inherit from Base
+        """
+
+        filename = cls.__name__ + ".json"
+        with open(filename, "w") as jsonfile:
+            if list_objs is None:
+                jsonfile.write("[]")
+            else:
+                list_dicts = [li.to_dictionary() for li in list_objs]
+                jsonfile.write(Base.to_json_string(list_dicts))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """
+        Returns the list of the JSON string representation json_string
+
+        Args:
+            json_string (str): A string representing a list of dictionaries
+        """
+
+        if json_string is None or json_string == "[]":
+            return []
+
+        return json.loads(json_string)
