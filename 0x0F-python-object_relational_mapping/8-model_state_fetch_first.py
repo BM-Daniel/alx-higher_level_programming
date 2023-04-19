4bd82b988ffcd5382b2e885d86392495fd866957
BM-Daniel <danbabereyir@gmail.com>
2023-04-19 14:06:58 +0000

    Work on task 6-10

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+"""
+Write a script that prints the first State object from the database
+hbtn_0e_6_usa
+
+Usage: ./file_name.py <mysql username> <mysql password> <database name>
+"""
+
+from model_state import Base, State
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = session.query(State).order_by(State.id).first()
+
+    if state is None:
+        print("nothing")
+    else:
+        print("{}: {}".format(state.id, state.name))
---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+"""
+Write a python file that contains the class definition of a State and an
+instance Base = declarative_base()
+You must use the module SQLAlchemy
+"""
+
+from sqlalchemy import Integer, String, Column
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class State(Base):
+    """
+    Class to represent a sql table called states
+
+    __tablename__ (str): Name of the sql table
+    id (sqlalchemy.Integer): The id of the state
+    name (sqlalchemy.String): The name of the state
+    """
+
+    __tablename__ = "states"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+    
