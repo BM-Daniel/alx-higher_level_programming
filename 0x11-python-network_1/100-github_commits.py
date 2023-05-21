3c4f453b8c62989ba1192aed40134213c7a457f2
BM-Daniel <danbabereyir@gmail.com>
2023-05-21 16:34:08 +0000

    Advanced task

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""
+A script to list the 10 most recent commits of a repo
+"""
+
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = "https://api.github.com/repos/{}/{}/commits".format(sys.argv[2], sys.argv[1])
+    req = requests.get(url)
+    repo_commits = req.json()
+
+    try:
+        for i in range(10):
+            print("{}: {}".format(repo_commits[i].get("sha"), repo_commits[i].get("commit").get("author").get("name")))
+    
+    except IndexError:
+        pass
