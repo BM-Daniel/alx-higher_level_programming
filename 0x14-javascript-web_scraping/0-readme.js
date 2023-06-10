97013243bcd746b1fa6a1231a7e798b260a55749
BM-Daniel <danbabereyir@gmail.com>
2023-06-10 22:18:14 +0000

    Semistandard

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 
-const fs = require('fs')
+const fs = require('fs');
 const filename = process.argv[2];
 
 fs.readFile(filename, 'utf-8', function (error, data) {
-    if (error) {
-        console.log(error)
-    } else {
-        console.log(data)
-    }
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(data);
+  }
 });
--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -5,7 +5,7 @@ const filename = process.argv[2];
 const info = process.argv[3];
 
 fs.writeFile(filename, info, 'utf-8', function (error) {
-    if (error) {
-        console.log(error);
-    }
+  if (error) {
+    console.log(error);
+  }
 });
